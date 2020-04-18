92f225bc907ea739bb245ef43858d4e6a5bc0277
sochacki111 <michalsochacki111@gmail.com>
2020-04-18 13:12:16 +0200

    Updating Person component state fixed

--- src/App.js
+++ src/App.js
@@ -21,14 +21,24 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Michaś111', age: 22 },
-        { name: event.target.value, age: 25 },
-        { name: 'Zdziś', age: 28 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    // Create copy object of found person
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    // Create copy of persons array 
+    const persons = [...this.state.persons];
+    // Update person in persons array
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
   deletePersonHandler = (personIndex) => {
@@ -78,7 +88,8 @@ class App extends Component {
                  * the different elements to find out which elements changed 
                  * and which didn't
                  */
-                key={person.id} />
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
             })
           }
         </div>
