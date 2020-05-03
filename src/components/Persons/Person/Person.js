5b02048488c0cc4c2b580e84b46e7426b5496c84
sochacki111 <michalsochacki111@gmail.com>
2020-05-03 11:57:22 +0200

    Changed Person and Persons functional components into Class based components. Reviewed some of Component Update Lifecycle for props change

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 /**
  * Thanks to webpack we can actually import css files into js
  * .css extension is needed. We can only omit it in .js files
  */
 import classes from './Person.css';
-const person = props => {
-  console.log('[Person.js] rendering...');
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click} > I'm {props.name} and I am {props.age} years old!</p>
-      {/** 
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
+        {/** 
        * Children refers to any elements (also plain text) between opening and closing tag of our component. 
        * It could be unordered list. It could be even any other React component 
        */}
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  )
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name} />
+      </div>
+    )
+  }
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 /**
  * Class based components are basicly used for state management
  * and functional components for the presentation of content
  */
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
-    return props.persons.map((person, index) =>
-        <Person
-            click={() => props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            /**
-             * Key property allows the React to keep track of the individual
-             * elements so that it has a clear property it can compare between
-             * the different elements to find out which elements changed 
-             * and which didn't
-             */
-            key={person.id}
-            changed={(event) => props.changed(event, person.id)} />
-    );
+class Persons extends Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
+
+    // componentWillReceiveProps(props) {
+    //     console.log('[Persons.js] componentWillReceiveProps', props);
+    // }
+   
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shoudComponentUpdate')
+        /**
+         * Here return true if react should continue updating or false if it shouldn't 
+         */
+        return true;
+    }    
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return { message: 'Snapshot!' };
+    }
+
+    // componentWillUpdate() {
+
+    // }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...');
+        return this.props.persons.map((person, index) =>
+            <Person
+                click={() => this.props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                /**
+                 * Key property allows the React to keep track of the individual
+                 * elements so that it has a clear property it can compare between
+                 * the different elements to find out which elements changed 
+                 * and which didn't
+                 */
+                key={person.id}
+                changed={(event) => this.props.changed(event, person.id)} />
+        );
+    }
 }
 
-export default persons;
+export default Persons;
