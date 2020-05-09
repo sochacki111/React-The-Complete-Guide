76d5f097397c701e136d49fd18e1aee7e4be84e3
sochacki111 <michalsochacki111@gmail.com>
2020-05-09 14:31:20 +0200

    Added prop-types package

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,27 +1,57 @@
+/**
+ * We can use Component.Fragment instead of Aux
+ * import React, { Component, Fragment } from 'react';
+ */
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types';
+
 /**
  * Thanks to webpack we can actually import css files into js
  * .css extension is needed. We can only omit it in .js files
  */
 import classes from './Person.css';
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  /**
+   * componetDidMount() executes after render()
+   */
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Aux>
+        {this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
         <p onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
         {/** 
-       * Children refers to any elements (also plain text) between opening and closing tag of our component. 
-       * It could be unordered list. It could be even any other React component 
-       */}
+         * Children refers to any elements (also plain text) between opening and closing tag of our component. 
+         * It could be unordered list. It could be even any other React component 
+        */}
         <p>{this.props.children}</p>
         <input
+          // ref={(inputEl) => { this.inputElement = inputEl }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
-          value={this.props.name} />
-      </div>
-    )
+          value={this.props.name}
+        />
+      </Aux>
+    );
   }
 };
 
-export default Person;
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
