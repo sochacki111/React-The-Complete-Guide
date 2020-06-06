32bf3dc79e3b138d2acbaea94be671c09443140f
sochacki111 <michalsochacki111@gmail.com>
2020-06-06 23:09:43 +0200

    Added layout component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 /**
  * Statefull component (this component manages state) aka "smart components" or "container components"
  * You only want them of a couple
@@ -9,7 +10,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary'
+import classes from './Layout.css'
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        {/* TODO Enable CSS Modules */}
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
