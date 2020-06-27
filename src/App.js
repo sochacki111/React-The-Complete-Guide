221af984920a0243393f3b1fb0b7017d73d4a781
sochacki111 <michalsochacki111@gmail.com>
2020-06-27 11:02:54 +0200

    Layout component created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './components/Layout/Layout'
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
